ec083051863a362250e699a797e4a0a2ded8676a
josefineMN <josefine.normann@awacademy.training>
2022-10-12 11:10:35 +0200

    Initial commit for setting up the project

---
+++ WarmupExercise.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class WarmupExercise {
+
+    public static void main(String[] args) {
+
+        private static int[] array1 = {1, 3, 4, 5, 6};
+        int array1 = new int ();
+
+        private static String[] array2 = { "", "", "", "", "" };
+        String array2 = new String();
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(String.format("Please enter a number (attempt: %d): ", attempts));
+        String numberEnteredByUser = scanner.nextLine();
+
+        if (String int: array1){
+
+        } else (String int: array1) {
+
+
+        System.out.println();
+    }
+}
